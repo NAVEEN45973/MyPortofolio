542ee05dcdb9c53e34bc615ac971615305a38fa1
NAVEEN45973 <srikakulapunaveen143@gmail.com>
2020-08-30 17:38:04 +0530

    Updates

---
+++ precache-manifest.99afdbb6292116306a806f0c2a81db94.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316f22e96fa299ae59b62e8498a36fcc",
+    "url": "/MyPortofolio/index.html"
+  },
+  {
+    "revision": "c5389b7c6a768a07e8a7",
+    "url": "/MyPortofolio/static/css/main.199375b8.chunk.css"
+  },
+  {
+    "revision": "b38f2c01dc66945a1719",
+    "url": "/MyPortofolio/static/js/2.7ca0ec87.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/MyPortofolio/static/js/2.7ca0ec87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5389b7c6a768a07e8a7",
+    "url": "/MyPortofolio/static/js/main.b48bdbde.chunk.js"
+  },
+  {
+    "revision": "9023b026327aa0c2266a",
+    "url": "/MyPortofolio/static/js/runtime-main.8ce0d1ad.js"
+  },
+  {
+    "revision": "8f6808856a72505ca5108c8d8edf0a40",
+    "url": "/MyPortofolio/static/media/background.8f680885.webp"
+  },
+  {
+    "revision": "a95f8a0cc6f7afee274b61ea02fe776b",
+    "url": "/MyPortofolio/static/media/background.a95f8a0c.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/MyPortofolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "65aa0b061fe9bf7f1f17b975e3958298",
+    "url": "/MyPortofolio/static/media/c.65aa0b06.svg"
+  },
+  {
+    "revision": "c64a068ead3cf3210c7608cea6018583",
+    "url": "/MyPortofolio/static/media/cpp.c64a068e.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/MyPortofolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/MyPortofolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/MyPortofolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/MyPortofolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/MyPortofolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "d77348fb508868c3c0ad99e2039a0d0c",
+    "url": "/MyPortofolio/static/media/laravel.d77348fb.svg"
+  },
+  {
+    "revision": "9145b92aeca0c9fe257088d3eab7fb59",
+    "url": "/MyPortofolio/static/media/logow.9145b92a.webp"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/MyPortofolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/MyPortofolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "44403f1eece67c892a0c6a6791da5ad4",
+    "url": "/MyPortofolio/static/media/php.44403f1e.svg"
+  },
+  {
+    "revision": "b02f03d41911136368b9e4fa66211d54",
+    "url": "/MyPortofolio/static/media/profile.b02f03d4.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/MyPortofolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "2a5335d52070f6ad653a12df2a36fdd2",
+    "url": "/MyPortofolio/static/media/pythonanywhere.2a5335d5.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/MyPortofolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/MyPortofolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/MyPortofolio/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "dc93f89a738f0d2c77c2361b3e2d6947",
+    "url": "/MyPortofolio/static/media/slide1.dc93f89a.webp"
+  },
+  {
+    "revision": "68c147aabb9795ca197c71fe1c862a69",
+    "url": "/MyPortofolio/static/media/slide2.68c147aa.webp"
+  },
+  {
+    "revision": "5c9e9ad88fa8cbfa3863c6f4f8b1b43f",
+    "url": "/MyPortofolio/static/media/slide3.5c9e9ad8.webp"
+  }
+]);
